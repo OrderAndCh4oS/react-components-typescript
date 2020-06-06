cbd7e5d2e68c1ff8760d99f68593b27bcfe186e7
Sarcoma <sarcoma@live.co.uk>
2020-06-06 14:41:11 +0100

    Add new demo

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -6,11 +6,11 @@ const App = () => {
   return (
     <ContainerFullWidth className={styles.bc_lighterGrey}>
       <Row>
-        <Column className={styles.col6}>
+        <Column columnClass={styles.col6}>
           <Title tag={'h1'}>Hello World</Title>
           <Text>Blah blah blah</Text>
         </Column>
-        <Column className={styles.col6}>
+        <Column columnClass={styles.col6}>
           <Title tag={'h1'}>Second Column</Title>
           <Text>Blah blah blah</Text>
         </Column>

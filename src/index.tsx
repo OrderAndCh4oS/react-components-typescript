186d0fe950fb52199ae01487e832f4c697d4f8cb
Sarcoma <sarcoma@live.co.uk>
2020-07-17 20:58:46 +0100

    add to index

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import styles from '@orderandchaos/react-styles/dist/styles.module.css'
 
 export {Container, ContainerFullWidth, Row, Column, Panel, Table} from './components/structure';
 export {Title, Pretitle, IntroText, Text, BlockQuote, Citation} from './components/typography';
-export {Field, Input, TextArea, Select, Switch, FormError} from './components/form';
+export {Label, FormField, Field, Input, TextArea, Select, Switch, FormError} from './components/form';
 export {Button, Link, LinkButton} from './components/button';
 export {Picture, Caption} from './components/media';
 export {styles};

70d8479eb5771074d0b69c3b727aac3626cc0d68
Sarcoma <sarcoma@live.co.uk>
2020-09-06 23:18:27 +0100

    Add Radio Buttons

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,22 +1,28 @@
-import React from 'react'
+import React from "react";
 
-import { Column, ContainerFullWidth, Row, styles, Text, Title } from '@orderandchaos/react-components-typescript'
+import { Column, ContainerFullWidth, Row, styles, Text, Title, Button, RadioButton } from "@orderandchaos/react-components-typescript";
 
 const App = () => {
   return (
     <ContainerFullWidth className={styles.bc_lighterGrey}>
       <Row>
         <Column columnClass={styles.col6}>
-          <Title tag={'h1'}>Hello World</Title>
+          <Title tag={"h1"}>Hello World</Title>
           <Text>Blah blah blah</Text>
         </Column>
         <Column columnClass={styles.col6}>
-          <Title tag={'h1'}>Second Column</Title>
+          <Title tag={"h1"}>Second Column</Title>
           <Text>Blah blah blah</Text>
+          <Button>Click Me!</Button>
+          <div>
+            <RadioButton checked={false}>One</RadioButton>
+            <RadioButton checked={true}>Two</RadioButton>
+            <RadioButton checked={false}>Three</RadioButton>
+          </div>
         </Column>
       </Row>
     </ContainerFullWidth>
-  )
-}
+  );
+};
 
-export default App
+export default App;
